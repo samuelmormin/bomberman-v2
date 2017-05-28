dc09157466b1b4573953c8e48e4bed9e019ea75e
KylitoH <kyle.hazan@gmail.com>
2017-05-28 20:16:54 +0200

    homepage done, add lives to the game set

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,7 +1,20 @@
 <div class="header">
     <img src="<?= URL ?>assets/img/logo.png" alt="">
 </div>
+<button id="myBtn">Voir les règles du jeu</button>
+<div id="myModal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <img src="assets/img/rules.png" alt="">
+    </div>
+</div>
+<div class="lives">
+    <div class="live1"><img src="assets/img/lives.svg" alt=""></div>
+    <div class="live2"><img src="assets/img/lives.svg" alt=""></div>
+    <div class="live3"><img src="assets/img/lives.svg" alt=""></div>
+</div>
 
+<script type="text/javascript" src="<?= URL ?>assets/js/start-play.js"></script>
 <script type="text/javascript" src="<?= URL ?>assets/js/game-set.js"></script>
 <script type="text/javascript" src="<?= URL ?>assets/js/player.js"></script>
 <script type="text/javascript" src="<?= URL ?>assets/js/a_i_player.js"></script>
--- views/pages/start_play.php
+++ views/pages/start_play.php
@@ -1,1 +1,18 @@
-<div class="hero"></div>
+<link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+<div class="hero">
+    <div class="container">
+        <img src="assets/img/logo.png" alt="logo_of_the_game">
+        <p>Jouez au Bomberman sur le thème du Hip Hop américain, détruisez vos adversaires en incarnant le célèbre Tupac.</p>
+        <button><a href="home">Jouer (Solo)</a></button>
+        <button><a href="home">Jouer (IA)</a></button>
+        <br>
+        <button id="myBtn">Voir les règles du jeu</button>
+        <div id="myModal" class="modal">
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <img src="assets/img/rules.png" alt="">
+            </div>
+        </div>
+    </div>
+    
+<script type="text/javascript" src="<?= URL ?>assets/js/start-play.js"></script>
